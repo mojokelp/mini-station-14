d51a8665e1279428d8ee5be1a8b10a5b3cd503ac
Tornado Tech <54727692+TornadoTechnology@users.noreply.github.com>
2025-07-09 16:28:51 +1000

    feat: map loader

---
+++ Content.Server/_TT/AdditionalMap/TTAdditionalMapPrototype.cs
@@ -0,0 +1,19 @@
+﻿/*
+ * License: MIT
+ * Copyright: (c) 2025 TornadoTechnology
+ */
+
+using Content.Server.Maps;
+using Robust.Shared.Prototypes;
+
+namespace Content.Server._TT.AdditionalMap;
+
+[Prototype("additionalMap")]
+public sealed class TTAdditionalMapPrototype : IPrototype
+{
+    [IdDataField]
+    public string ID { get; } = string.Empty;
+
+    [DataField("maps")]
+    public List<ProtoId<GameMapPrototype>> MapProtoIds = new();
+}
