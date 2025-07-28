6ab43b9195f79966f1fa02c909771ef0f4823061
Tornado Tech <54727692+TornadoTechnology@users.noreply.github.com>
2025-07-28 17:21:29 +1000

    feat: Station handle job

---
+++ Content.Server/_TT/StationHandleJob/TTStationHandleJobComponent.cs
@@ -0,0 +1,12 @@
+﻿using Content.Shared.Roles;
+using Robust.Shared.GameStates;
+using Robust.Shared.Prototypes;
+
+namespace Content.Server._TT.StationHandleJob;
+
+[RegisterComponent, NetworkedComponent, AutoGenerateComponentState]
+public sealed partial class TTStationHandleJobComponent : Component
+{
+    [DataField, AutoNetworkedField]
+    public List<ProtoId<JobPrototype>> Jobs = new();
+}
---
+++ Content.Server/_TT/StationHandleJob/TTStationHandleJobSystem.cs
@@ -0,0 +1,100 @@
+﻿using Content.Server.GameTicking;
+using Content.Server.Shuttles.Systems;
+using Content.Server.Spawners.Components;
+using Content.Server.Spawners.EntitySystems;
+using Content.Server.Station.Systems;
+using Content.Shared.Roles;
+using Robust.Server.Containers;
+using Robust.Shared.Containers;
+using Robust.Shared.Prototypes;
+using Robust.Shared.Random;
+
+namespace Content.Server._TT.StationHandleJob;
+
+public sealed class TTStationHandleJobSystem : EntitySystem
+{
+    [Dependency] private readonly IRobustRandom _random = default!;
+    [Dependency] private readonly GameTicker _gameTicker = default!;
+    [Dependency] private readonly StationSpawningSystem _stationSpawning = default!;
+    [Dependency] private readonly StationSystem _station = default!;
+    [Dependency] private readonly ContainerSystem _container = default!;
+
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        SubscribeLocalEvent<PlayerSpawningEvent>(OnPlayerSpawning, before: [typeof(ArrivalsSystem), typeof(ContainerSpawnPointSystem)]);
+    }
+
+  private void OnPlayerSpawning(PlayerSpawningEvent ev)
+    {
+        if (ev.SpawnResult is not null || ev.Job is not {} job)
+            return;
+
+        if (GetStation(job) is not {} stationUid)
+            return;
+
+        var query = EntityQueryEnumerator<ContainerSpawnPointComponent, ContainerManagerComponent, TransformComponent>();
+        var possibleContainers = new List<Entity<ContainerSpawnPointComponent, ContainerManagerComponent, TransformComponent>>();
+        while (query.MoveNext(out var uid, out var spawnPoint, out var container, out var xform))
+        {
+            if (_station.GetOwningStation(uid, xform) != stationUid)
+                continue;
+
+            // If it's unset, then we allow it to be used for both roundstart and midround joins
+            if (spawnPoint.SpawnType == SpawnPointType.Unset)
+            {
+                // Make sure we also check the job here for various reasons.
+                if (spawnPoint.Job != job)
+                    continue;
+
+                possibleContainers.Add((uid, spawnPoint, container, xform));
+            }
+
+            if (_gameTicker.RunLevel == GameRunLevel.InRound && spawnPoint.SpawnType == SpawnPointType.LateJoin)
+                possibleContainers.Add((uid, spawnPoint, container, xform));
+
+            if (_gameTicker.RunLevel != GameRunLevel.InRound && spawnPoint.SpawnType == SpawnPointType.Job && spawnPoint.Job == job)
+                possibleContainers.Add((uid, spawnPoint, container, xform));
+        }
+
+        if (possibleContainers.Count == 0)
+            return;
+
+        var baseCoords = possibleContainers[0].Comp3.Coordinates;
+        ev.SpawnResult = _stationSpawning.SpawnPlayerMob(
+            baseCoords,
+            job,
+            ev.HumanoidCharacterProfile,
+            stationUid);
+
+        _random.Shuffle(possibleContainers);
+        foreach (var (uid, spawnPoint, manager, xform) in possibleContainers)
+        {
+            if (!_container.TryGetContainer(uid, spawnPoint.ContainerId, out var container, manager))
+                continue;
+
+            if (!_container.Insert(ev.SpawnResult.Value, container, containerXform: xform))
+                continue;
+
+            return;
+        }
+
+        Del(ev.SpawnResult);
+        ev.SpawnResult = null;
+    }
+
+    private EntityUid? GetStation(ProtoId<JobPrototype> job)
+    {
+        var query = EntityQueryEnumerator<TTStationHandleJobComponent>();
+        while (query.MoveNext(out var uid, out var component))
+        {
+            if (!component.Jobs.Contains(job))
+                continue;
+
+            return uid;
+        }
+
+        return null;
+    }
+}
