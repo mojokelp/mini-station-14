cc8578581da2073d06a5c4082c867e2682d4cac7
Egorick1845 <proegorweb@gmail.com>
2025-08-02 14:00:14 +0300

    хуй

--- Content.Server/_TT/StationHandleJob/TTStationHandleJobSystem.cs
+++ Content.Server/_TT/StationHandleJob/TTStationHandleJobSystem.cs
@@ -6,6 +6,7 @@ using Content.Server.Station.Systems;
 using Content.Shared.Roles;
 using Robust.Server.Containers;
 using Robust.Shared.Containers;
+using Robust.Shared.Map;
 using Robust.Shared.Prototypes;
 using Robust.Shared.Random;
 
@@ -23,7 +24,7 @@ public sealed class TTStationHandleJobSystem : EntitySystem
     {
         base.Initialize();
 
-        SubscribeLocalEvent<PlayerSpawningEvent>(OnPlayerSpawning, before: [typeof(ArrivalsSystem), typeof(ContainerSpawnPointSystem)]);
+        SubscribeLocalEvent<PlayerSpawningEvent>(OnPlayerSpawning, before: [typeof(ArrivalsSystem), typeof(ContainerSpawnPointSystem), typeof(SpawnPointSystem)]);
     }
 
   private void OnPlayerSpawning(PlayerSpawningEvent ev)
@@ -34,54 +35,33 @@ public sealed class TTStationHandleJobSystem : EntitySystem
         if (GetStation(job) is not {} stationUid)
             return;
 
-        var query = EntityQueryEnumerator<ContainerSpawnPointComponent, ContainerManagerComponent, TransformComponent>();
-        var possibleContainers = new List<Entity<ContainerSpawnPointComponent, ContainerManagerComponent, TransformComponent>>();
-        while (query.MoveNext(out var uid, out var spawnPoint, out var container, out var xform))
+        var query = EntityQueryEnumerator<SpawnPointComponent, TransformComponent>();
+        var possiblePositions = new List<EntityCoordinates>();
+
+        while (query.MoveNext(out var uid, out var spawnPoint, out var xform))
         {
             if (_station.GetOwningStation(uid, xform) != stationUid)
                 continue;
 
-            // If it's unset, then we allow it to be used for both roundstart and midround joins
-            if (spawnPoint.SpawnType == SpawnPointType.Unset)
-            {
-                // Make sure we also check the job here for various reasons.
-                if (spawnPoint.Job != job)
-                    continue;
-
-                possibleContainers.Add((uid, spawnPoint, container, xform));
-            }
+            if (spawnPoint.Job != job)
+                continue;
 
             if (_gameTicker.RunLevel == GameRunLevel.InRound && spawnPoint.SpawnType == SpawnPointType.LateJoin)
-                possibleContainers.Add((uid, spawnPoint, container, xform));
+                possiblePositions.Add(xform.Coordinates);
 
-            if (_gameTicker.RunLevel != GameRunLevel.InRound && spawnPoint.SpawnType == SpawnPointType.Job && spawnPoint.Job == job)
-                possibleContainers.Add((uid, spawnPoint, container, xform));
+            if (_gameTicker.RunLevel != GameRunLevel.InRound && spawnPoint.SpawnType == SpawnPointType.Job)
+                possiblePositions.Add(xform.Coordinates);
         }
 
-        if (possibleContainers.Count == 0)
+        if (possiblePositions.Count == 0)
             return;
 
-        var baseCoords = possibleContainers[0].Comp3.Coordinates;
+        var spawnLoc = _random.Pick(possiblePositions);
         ev.SpawnResult = _stationSpawning.SpawnPlayerMob(
-            baseCoords,
+            spawnLoc,
             job,
             ev.HumanoidCharacterProfile,
             stationUid);
-
-        _random.Shuffle(possibleContainers);
-        foreach (var (uid, spawnPoint, manager, xform) in possibleContainers)
-        {
-            if (!_container.TryGetContainer(uid, spawnPoint.ContainerId, out var container, manager))
-                continue;
-
-            if (!_container.Insert(ev.SpawnResult.Value, container, containerXform: xform))
-                continue;
-
-            return;
-        }
-
-        Del(ev.SpawnResult);
-        ev.SpawnResult = null;
     }
 
     private EntityUid? GetStation(ProtoId<JobPrototype> job)
